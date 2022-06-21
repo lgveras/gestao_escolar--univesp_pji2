47c06d27b2b0f791d303cd8c0e6c463144c555ab
lgveras <luiizgustavo@gmail.com>
2022-06-21 11:36:45 -0300

    Página de Perfil do professor e links de menu corrigidos

--- escolar/professor_views.py
+++ escolar/professor_views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render, redirect
+from .models import Professor 
 
-def home(request, prof_id):
+def home(request, prof_matricula):
     context = {'first_name': 'Johnny'}
     return render(request, 'professor_template/base_template.html', context)
 
-def profile(request, prof_id):
-    context = {'first_name': 'Johnny'}
-    return render(request, 'professor_template/base_template.html', context)
+def profile(request, prof_matricula):
+    # ETAPAS DE PROFILE
+    # 1. Buscar dados pelo model de Professor
+    professor = Professor.objects.get(cadastro_fk=prof_matricula)
+    # 2. Preencher o context
+    context = {
+        'prof_matricula': professor.cadastro_fk.matricula,
+        'prof_nome': professor.cadastro_fk.nome,
+        'prof_email': professor.cadastro_fk.email,
+        'prof_telefone': professor.cadastro_fk.telefone,
+        'prof_area': professor.area_conhecimento_fk.descricao,
+    }
+    # 3. Passar para o template do profile
+    return render(request, 'professor_template/profile.html', context)
 
 # Endpoint que busca todas as disciplinas de um professor
-def disciplinas(request, prof_id):
+def disciplinas(request, prof_matricula):
     # database search
     # TODO implements
     context = {'first_name': 'Johnny'}
-    return render(request, 'staff_template/staff_profile.html', context)
+    return render(request, 'professor_template/disciplinas.html', context)
 
 # Endpoint que busca os dados de uma disciplina de um professor
 def disciplina_select(request, prof_id, disciplina_id):
--- escolar/urls.py
+++ escolar/urls.py
@@ -10,16 +10,17 @@ urlpatterns = [
     
     # URLs para funções de professor
     # TODO: Retirar o id do professor do PATH. Quando ele logar, o id deve ficar armazenada em cookie ou algo do gênero
-    path('prof/<int:prof_id>/home/', professor_views.home, name="professor_home"),
+    path('prof/<str:prof_matricula>/home/', professor_views.home, name="professor-home"),
     # TODO: Retirar o id do professor do PATH. Quando ele logar, o id deve ficar armazenada em cookie ou algo do gênero
-    path('prof/<int:prof_id>/profile/', professor_views.profile, name="professor_profile"),
+    path('prof/<str:prof_matricula>/profile/', professor_views.profile, name="professor-profile"),
     # Página que listará todas as disciplinas
-    path('prof/<int:prof_id>/disciplinas/', professor_views.disciplinas, name="professor_disciplinas"),
+    path('prof/<str:prof_matricula>/disciplinas/', professor_views.disciplinas, name="professor-disciplinas"),
     # Página que listará dados de uma disciplina
-    path('prof/<int:prof_id>/disciplinas/<int:disciplina_id>', professor_views.disciplina_select, name="disciplina_selecionada"),
+    path('prof/<int:prof_id>/disciplinas/<int:disciplina_id>', professor_views.disciplina_select, name="disciplina-selecionada"),
     # Página que listará os alunos de uma disciplina. Será possível alterar as datas
-    path('prof/<int:prof_id>/disciplinas/<int:disciplina_id>/alunos', professor_views.disciplina_alunos, name="disciplina_alunos"),
+    path('prof/<int:prof_id>/disciplinas/<int:disciplina_id>/alunos', professor_views.disciplina_alunos, name="disciplina-alunos"),
       
     # URLs para funcionalidades de gestor
-    path('gestor_home/', gestor_views.home, name="gestor_home"),
+    path('gestor_home/', gestor_views.home, name="gestor-home"),
+    # Configurar as outras rotas de gestor
 ]
