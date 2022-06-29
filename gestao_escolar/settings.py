78134b36182bcd0bad1bbcd88a53c82d876463ef
lgveras <luiizgustavo@gmail.com>
2022-06-29 14:00:20 -0300

    Static files

--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # STATICFILES_DIRS = [
 #   Path.joinpath(BASE_DIR, 'static/'),
